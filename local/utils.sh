0f95f120c30d855d465e86af2d8535a93d8c03bd
avriis <github@diri.dk>
2022-01-28 19:35:36 +0100

    Update of utils

--- local/utils.sh
+++ local/utils.sh
@@ -2,11 +2,9 @@
 
 _script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-export DJANGO_SETTINGS_MODULE=djangofir.settings.development
-
 function ofir {
   local _venv_name=venv
-  local _venv_dir="python/$_venv_name"
+  local _venv_dir="$_script_dir/../python/$_venv_name"
   local _django_name=djangofir
   local _this=${FUNCNAME[0]}
   local _nextjs_name="nextjs"
@@ -14,30 +12,53 @@ function ofir {
     echo -e "\n>>> running: $*\n" && eval "$@"
   }
   function _manage_py {
-    local _pm="$_script_dir/../$_venv_dir/bin/python manage.py"
-    local _app_name="jobs"
+    local _pm="$_venv_dir/bin/python manage.py"
+    _print_and_do "$_this set-env dev"
     cd "$_django_name" || return
     case "$1" in
       rs) _print_and_do "$_pm runserver";;
-      startmyapp) _print_and_do "$_pm startapp $_app_name";;
       *) _print_and_do "$_pm $*";;
     esac
     cd - || return
   }
   function _setup {
+    echo "Deprecated"
+    ## case "$1" in
+    ##   clean) _print_and_do "rm -rf $_venv_dir $_django_name $_nextjs_name";;
+    ##   venv) _print_and_do "python3 -m venv $_venv_dir";;
+    ##   mypip) _print_and_do "$_venv_dir/bin/pip install -r python/requirements.txt";;
+    ##   pip) _print_and_do "$_venv_dir/bin/pip ${*:2}";;
+    ##   django) _print_and_do "$_venv_dir/bin/django-admin startproject $_django_name" && _print_and_do "$_this pm startmyapp";;
+    ##   nextjs) _print_and_do "npx create-next-app --use-npm $_nextjs_name";;
+    ##   *) echo "unknown command";;
+    ## esac
+  }
+  function _venv {
+    _print_and_do "$_venv_dir/bin/$*"
+  }
+  function _set_env {
     case "$1" in
-      clean) _print_and_do "rm -rf $_venv_dir $_django_name $_nextjs_name";;
-      venv) _print_and_do "python3 -m venv $_venv_dir";;
-      mypip) _print_and_do "$_venv_dir/bin/pip install -r python/requirements.txt";;
-      pip) _print_and_do "$_venv_dir/bin/pip ${*:2}";;
-      django) _print_and_do "$_venv_dir/bin/django-admin startproject $_django_name" && _print_and_do "$_this pm startmyapp";;
-      nextjs) _print_and_do "npx create-next-app --use-npm $_nextjs_name";;
-      *) "$_this" setup venv && "$_this" setup mypip && "$_this" setup django && "$_this" setup nextjs;;
+      dev) _print_and_do "export DJANGO_SETTINGS_MODULE=$_django_name.settings.development";;
+      test) _print_and_do "export DJANGO_SETTINGS_MODULE=$_django_name.settings.testing";;
+      *) echo "unknown command";;
     esac
   }
+  function _test {
+    _print_and_do "$_this set-env test"
+    cd "$_django_name" || return
+    "$_venv_dir/bin/pytest" 
+    cd - || return
+  }
+  function _next {
+    _print_and_do "cd $_nextjs_name"
+    _print_and_do "npm run dev"
+  }
   case "$1" in
-    setup) _setup "${@:2}";;
+    venv) _venv "${@:2}";;
+    set-env) _set_env "${@:2}";;
     pm) _manage_py "${@:2}";;
+    next) _next "${@:2}";;
+    test) _test "${@:2}";;
     *) echo "unknown command";;
   esac
   }
@@ -53,12 +74,12 @@ function _ofir_completions {
       COMPREPLY=("${MAPFILE[@]}")
     fi
   }
-  if [[ "${COMP_WORDS[1]}" == "setup" ]]; then
-    _set_compreply "clean venv pip mypip django nextjs" "${COMP_WORDS[2]}"
-  elif [[ "${COMP_WORDS[1]}" == "pm" ]]; then
+  if [[ "${COMP_WORDS[1]}" == "pm" ]]; then
     _set_compreply "rs" "${COMP_WORDS[2]}"
+  elif [[ "${COMP_WORDS[1]}" == "set-env" ]]; then
+    _set_compreply "dev test" "${COMP_WORDS[2]}"
   else
-    _set_compreply "setup pm" "${COMP_WORDS[1]}"
+    _set_compreply "next test pm set-env venv" "${COMP_WORDS[1]}"
   fi
 }
 
