50972cb535b2608f010367ef4da4010d6a6a25a5
avriis <github@diri.dk>
2022-01-28 20:58:11 +0100

    Add unit test

--- djangofir/jobs/tests.py
+++ djangofir/jobs/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from jobs.models import Job
+from users.models import User
 
-# Create your tests here.
+
+class JobTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(name="test user")
+        Job.objects.create(title="Cool job", body="Body text", user=user)
+
+    def test_jobs(self):
+        job = Job.objects.get(title="Cool job")
+        self.assertEqual(job.body, 'Body text')
