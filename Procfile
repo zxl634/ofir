a8072841eb55e3f7be8a84ec5b363cecb0d10e59
avriis <github@diri.dk>
2022-01-29 10:21:59 +0100

    Add heroku credentials

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djangofir.wsgi
--- djangofir/djangofir/settings/production.py
+++ djangofir/djangofir/settings/production.py
@@ -1,8 +1,11 @@
 from .base import *  # noqa
 import environ
+import django_heroku
 
 
 env = environ.Env()
 SECURE_SSL_REDIRECT = True
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 STATIC_ROOT = env("STATIC_ROOT")
+
+django_heroku.settings(locals())
